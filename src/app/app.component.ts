3a78a659802519ae54327bb9287406270aff949e
Karla Fernandes <karla@vitaminak.com.br>
2017-06-27 13:01:14 -0300

    Changing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {MailService} from "./mail.service";
 
 @Component({
   selector: 'app-root',
-  template: '<div><app-simple-form></app-simple-form></div>',
-  styleUrls: ['./app.component.css']
+  template: '<div><app-simple-form></app-simple-form>{{mail.message}}</div>'
 })
 export class AppComponent {
   title = 'Lets get started';
+
+  constructor(private mail:MailService){}
 }
