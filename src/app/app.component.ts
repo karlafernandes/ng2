bcd661162ca335e2e9f57c07802ef6314abc7eda
Karla Fernandes <karla@vitaminak.com.br>
2017-06-27 12:28:02 -0300

    Creating simple-form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<div><app-simple-form></app-simple-form></div>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
