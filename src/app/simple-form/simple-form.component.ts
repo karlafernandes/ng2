5932866507bfa4d1ba97d270cba1616aa7a40ce7
Karla Fernandes <karla@vitaminak.com.br>
2017-06-27 12:36:26 -0300

    Binding input with button

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -2,10 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
-  template: '<div><input type="text"><button>Click me!</button></div>',
+  template: '<div><input #myInput type="text"><button (click)="onClick(myInput.value)">Click me!</button></div>',
   styleUrls: ['./simple-form.component.css']
 })
 export class SimpleFormComponent implements OnInit {
+
+  onClick(value) {
+    console.log(value);
+  }
 
   constructor() { }
 
