c569004f1aa949420d352ebc3c43fea2caa27bfd
Karla Fernandes <karla@vitaminak.com.br>
2017-06-28 11:03:41 -0300

    Encapsulation or Global styles

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation} from '@angular/core';
 @Component({
-    encapsulation: ViewEncapsulation.Native, /* when you don' want to styles global to be applied.*/
+    // when you don' want to styles global to be applied. encapsulation: ViewEncapsulation.Native,
     selector: 'app-simple-form',
     template: '<input #myInput type="text" [ngClass]="{mousedown:isMouseDown}" (mousedown)="isMouseDown=true" (mouseup)="isMouseDown=false" (mouseleave)="isMouseDown=false" [(ngModel)]="message"><button (click)="update.emit({text:message})">Click me!</button>',
     styles: [':host {display:flex; flex-diration: column;} input:focus {font-weight: bold; outline: none;} button { border: none;} .mousedown { border: 2px solid green;}']
