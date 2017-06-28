321a97e367cc8ce68c13dd1044c92ec28de909bb
Karla Fernandes <karla@vitaminak.com.br>
2017-06-28 10:55:37 -0300

    Applying CSS conditionally

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,19 +1,22 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
-  selector: 'app-simple-form',
-  template: '<input #myInput type="text" [(ngModel)]="message"><button (click)="update.emit({text:message})">Click me!</button>',
-  styles: [':host {display:flex; flex-diration: column;} input:focus {font-weight: bold; outline: none;} button { border: none;}']
+    selector: 'app-simple-form',
+    template: '<input #myInput type="text" [ngClass]="{mousedown:isMouseDown}" (mousedown)="isMouseDown=true" (mouseup)="isMouseDown=false" (mouseleave)="isMouseDown=false" [(ngModel)]="message"><button (click)="update.emit({text:message})">Click me!</button>',
+    styles: [':host {display:flex; flex-diration: column;} input:focus {font-weight: bold; outline: none;} button { border: none;} .mousedown { border: 2px solid green;}']
 })
 export class SimpleFormComponent implements OnInit {
 
-  @Input() message;
+    isMouseDown;
 
-  @Output() update = new EventEmitter();
+    @Input() message;
 
-  constructor() { }
+    @Output() update = new EventEmitter();
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
